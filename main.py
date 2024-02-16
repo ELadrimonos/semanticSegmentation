d1140447b07384df73c6cd457ab1ee5deab9e437
Adrián Primo <adrianprimob@gmail.com>
2024-02-16 11:55:01 +0100

    Parte 2 - Comienzo

--- main.py
+++ main.py
@@ -1,36 +1,48 @@
 from transformers import SegformerImageProcessor, AutoModelForSemanticSegmentation
 from PIL import Image
-import requests
 import matplotlib.pyplot as plt
 import torch.nn as nn
+import gradio as gr
+import numpy as np
 
 # Cargar el procesador y el modelo
 processor = SegformerImageProcessor.from_pretrained('mattmdjaga/segformer_b2_clothes')
 model = AutoModelForSemanticSegmentation.from_pretrained('mattmdjaga/segformer_b2_clothes')
 
-# Cargar la imagen
-url = 'https://th.bing.com/th/id/OIP.I6Bn9EKsT2X-fBYMwohZXAHaLH?rs=1&pid=ImgDetMain'
-image = Image.open(requests.get(url, stream=True).raw)
 
-# Procesar la imagen
-inputs = processor(images=image, return_tensors='pt', size=512)
+def segmentar_imagen(image):
+    global model, processor
+    image = Image.fromarray(image)
+    # Procesar la imagen
+    inputs = processor(images=image, return_tensors='pt')
 
-# Realizar la inferencia
-model = model.eval()
-outputs = model(**inputs)
+    # Realizar la inferencia
+    model = model.eval()
+    outputs = model(**inputs)
 
-# Obtener los logits y realizar la interpolación
-logits = outputs.logits
-upsampled_logits = nn.functional.interpolate(
-    logits,
-    size=image.size[::-1],
-    mode='bilinear',
-    align_corners=False
-)
+    # Obtener los logits y realizar la interpolación
+    logits = outputs.logits.cpu()
+    upsampled_logits = nn.functional.interpolate(
+        logits,
+        size=image.size[::-1],
+        mode='bilinear',
+        align_corners=False
+    )
 
-# Obtener la segmentación predicha
-pred_seg = upsampled_logits.argmax(dim=1)[0]
+    # Obtener la segmentación predicha
+    pred_seg = upsampled_logits.argmax(dim=1)[0].numpy()
 
-# Visualizar la segmentación
-plt.imshow(pred_seg)
-plt.show()
+    colormap = plt.get_cmap('tab20').colors
+    colormap = np.array(colormap) * 255
+    colored_seg = np.zeros_like((*pred_seg.shape, 3), dtype=np.uint8)
+    for label in range(18):
+        # IndexError: too many indices for array: array is 1-dimensional, but 2 were indexed
+        colored_seg[pred_seg == label] = colormap[label]
+    return colored_seg
+
+
+gr_image_input = gr.Image(image_mode='RGB', type='numpy')
+gr_image_output = gr.Image(type='numpy', label='Segmented Image')
+
+demo = gr.Interface(fn=segmentar_imagen, inputs=gr_image_input, outputs=gr_image_output)
+demo.launch()
